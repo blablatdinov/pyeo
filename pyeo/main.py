42188740e408759578ebad4d3c6b6ce14e07f0f1
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2023-07-22 21:41:55 +0300

    Refactoring

--- pyeo/main.py
+++ pyeo/main.py
@@ -12,23 +12,27 @@ def _has_protocol(ctx) -> bool:
 
 
 def _each_method_has_protocol(ctx) -> bool:
-    methods_list = [
-        def_body
+    object_methods = {
+        def_body.name: def_body
         for def_body in ctx.cls.defs.body
         if not def_body.name.startswith('_')
-    ]
+    }
     if not ctx.cls.base_type_exprs:
         return False
-    protocol_methods = [
-        method
+    protocol_methods = set(
+        method.name
         for base_type in ctx.cls.base_type_exprs
         for node in base_type.node.mro
         for method in node.defn.defs.body
-    ]
-    extra_method_names = set(m.name for m in methods_list) - set(m.name for m in protocol_methods)
+    )
+    extra_method_names = set(object_methods.keys()) - protocol_methods
     if extra_method_names:
-        for extra_method_name in extra_method_names:
-            method = [x for x in methods_list if extra_method_name == x.name][0]
+        failed_methods = [
+            method
+            for method_name, method in object_methods.items()
+            if method_name in extra_method_names
+        ]
+        for method in failed_methods:
             ctx.api.fail(
                 "Class '{0}' have public extra method '{1}' without protocol.".format(
                     ctx.cls.name,
