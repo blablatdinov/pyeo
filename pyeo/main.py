25fbe7a484c8251f5acbc59416e50ba163f8f0de
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2023-07-23 14:45:54 +0300

    Protocols method code free

--- pyeo/main.py
+++ pyeo/main.py
@@ -1,5 +1,6 @@
 """No object without interface."""
 from mypy.plugin import Plugin
+from mypy.nodes import EllipsisExpr, PassStmt, StrExpr
 
 
 def _has_protocol(ctx) -> bool:
@@ -43,7 +44,22 @@ def _each_method_has_protocol(ctx) -> bool:
     return True
 
 
+def _protocol_method_code_free(ctx):
+    for method in ctx.cls.defs.body:
+        for body_item in method.body.body:
+            if isinstance(body_item, PassStmt):
+                continue
+            if not hasattr(body_item, 'expr'):
+                ctx.api.fail("Protocol '{0}' method '{1}' has implementation".format(ctx.cls.name, method.name), ctx.cls)
+                continue
+            if not isinstance(body_item.expr, (EllipsisExpr, StrExpr)):
+                ctx.api.fail("Protocol '{0}' method '{1}' has implementation".format(ctx.cls.name, method.name), ctx.cls)
+
+
 def analyze(ctx):
+    if ctx.cls.removed_base_type_exprs and ctx.cls.removed_base_type_exprs[0].fullname == 'typing.Protocol':
+        _protocol_method_code_free(ctx)
+        return True
     if not _has_protocol(ctx):
         ctx.api.fail("Class '{0}' does not implement a Protocol.".format(ctx.cls.name), ctx.cls)
         return True
