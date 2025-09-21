6a0990e9ddae3a6a013a4e8ad67e9965c941f7f7
Ilaletdinov Almaz <45946541+blablatdinov@users.noreply.github.com>
2025-09-21 13:59:32 +0300

    No getters (#74)

--- pyeo/main.py
+++ pyeo/main.py
@@ -30,7 +30,9 @@ from flake8.options.manager import OptionManager
 
 from pyeo.features.code_free_ctor_visitor import CodeFreeCtorVisitor
 from pyeo.features.no_er_suffix import NoErSuffix
+from pyeo.features.no_getter_methods import NoGetterMethodsVisitor
 from pyeo.features.no_mutable_objects import NoMutableObjectsVisitor
+from pyeo.features.no_property_decorator import NoPropertyDecoratorVisitor
 from pyeo.features.no_public_attributes import NoPublicAttributesVisitor
 
 
@@ -51,6 +53,8 @@ class Plugin:
             NoMutableObjectsVisitor(self._options),
             NoErSuffix(self._options),
             NoPublicAttributesVisitor(self._options),
+            NoPropertyDecoratorVisitor(self._options),
+            NoGetterMethodsVisitor(self._options),
         ]
 
     @classmethod
